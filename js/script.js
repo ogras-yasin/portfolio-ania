cec67ad1277a985045596b84f80d9f011e200fe3
Yasin Ogras <gs_yasin@hotmail.fr>
2023-06-12 22:08:12 +0200

    first commit a

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*==================== toggle icon navbar ====================*/
+
+/*==================== scroll sections active link ====================*/
+
+window.onscroll = () => {
+  /*==================== sticky navbar ====================*/
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+};
+
+/*==================== scroll reveal ====================*/
+
+/*==================== typed js ====================*/
